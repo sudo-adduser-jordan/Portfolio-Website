6ea209456ef47c6929923fcb419a2dd1fe432d87
[jordan] <108599131+sudo-adduser-jordan@users.noreply.github.com>
2023-03-10 16:06:15 -0800

    commit

--- components/components/ProjectGrid.tsx
+++ components/components/ProjectGrid.tsx
@@ -10,27 +10,10 @@ const ProjectGrid = () => {
 
     <div className={styles.grid}>
 
-      <a href='' className={styles.card}>
-        <h2>Portfolio Website &rarr;</h2>
-        <p>Portfolio website created with Javascript CSS3 HTML5. Hosted on Google Cloud Compute Engine with a Ubunutu Nginx server.</p>
-        <div className={styles.cardIcon}>
-            <Image src={'/googlecloud.svg'} className={styles.icon} width={25} height={25} alt='' />
-            <Image src={'/ubuntu.svg'} className={styles.icon} width={25} height={25} alt='' />
-            <Image src={'/nginx.svg'} className={styles.icon} width={25} height={25} alt='' />
-            <Image src={'/git.svg'} className={styles.icon} width={25} height={25} alt='' />
-            <Image src={'/javascript.svg'} className={styles.icon} width={25} height={25} alt='' />
-            <Image src={'/html5.svg'} className={styles.icon} width={25} height={25} alt='' />
-            <Image src={'/css3.svg'} className={styles.icon} width={25} height={25} alt='' />
-          </div>
-      </a>
-
-      <a href='' className={styles.card}>
+    <a href='' className={styles.card}>
         <h2>Portfolio Website (This Website) &rarr;</h2>
-        <p>Portfolio website created with typesafe Node, React, and Next.js.  Hosted on Google Cloud Compute Engine with a Ubunutu Nginx server.</p>
+        <p>agsdag sdag sag dsgdsagasd gasdgsdagsdagdsag. dfasdf dsaf df df ds fdfsdf sf ds.ef easf eaf efasfeasfaef gagergfsd</p>
         <div className={styles.cardIcon}>
-            <Image src={'/googlecloud.svg'} className={styles.icon} width={25} height={25} alt='' />
-            <Image src={'/ubuntu.svg'} className={styles.icon} width={25} height={25} alt='' />
-            <Image src={'/nginx.svg'} className={styles.icon} width={25} height={25} alt='' />
             <Image src={'/nodedotjs.svg'} className={styles.icon} width={25} height={25} alt='' />
             <Image src={'/npm.svg'} className={styles.icon} width={25} height={25} alt='' />
             <Image src={'/git.svg'} className={styles.icon} width={25} height={25} alt='' />
@@ -43,63 +26,91 @@ const ProjectGrid = () => {
       </a>
 
       <a href='' className={styles.card}>
-        <h2>Linux Server &rarr;</h2>
-        <p>A configuration guide for Google Cloud hosted Ubuntu virtual servers. Included technologies Apache, Nginx, Node, Python, Java.</p>
-        <Image src={'/googlecloud.svg'} className={styles.icon} width={25} height={25} alt='' />
-            <Image src={'/ubuntu.svg'} className={styles.icon} width={25} height={25} alt='' />
-            <Image src={'/nginx.svg'} className={styles.icon} width={25} height={25} alt='' />
+        <h2>Code Fragments - Website &rarr;</h2>
+        <p>agsdag sdag sag dsgdsagasd gasdgsdagsdagdsag. dfasdf dsaf df df ds fdfsdf sf ds.ef easf eaf efasfeasfaef gagergfsd</p>
+            <Image src={'/git.svg'} className={styles.icon} width={25} height={25} alt='' />
             <Image src={'/nodedotjs.svg'} className={styles.icon} width={25} height={25} alt='' />
             <Image src={'/npm.svg'} className={styles.icon} width={25} height={25} alt='' />
-      </a>
-
-      <a href='' className={styles.card}>
-        <h2>Java Banking Application &rarr;</h2>
-        <p>Desktop Graphical User Interface finance tracking application created with Spring.asdfg dsagfegf asdge4agsdg dgsdag sda gdsg asdgeesagfdse gtgasgfds</p>
-            <Image src={'/windows.svg'} className={styles.icon} width={25} height={25} alt='' />
-            <Image src={'/vscode.svg'} className={styles.icon} width={25} height={25} alt='' />
-            <Image src={'/spring.svg'} className={styles.icon} width={25} height={25} alt='' />
-            <Image src={'/mysql.svg'} className={styles.icon} width={25} height={25} alt='' />
-      </a>
-
-      <a href='' className={styles.card}>
-        <h2>Java Encryption/Decryption &rarr;</h2>
-        <p>Simple Java command line program that will encode and decode your text. jkfldjsa;fjd safjdlskaf dsklajf dsjfsd afdsfsdlkafjldsa dsfafasfj asds;fjdsa f</p>
-            <Image src={'/windows.svg'} className={styles.icon} width={25} height={25} alt='' />
-            <Image src={'/vscode.svg'} className={styles.icon} width={25} height={25} alt='' />
-            <Image src={'/spring.svg'} className={styles.icon} width={25} height={25} alt='' />
-            <Image src={'/mysql.svg'} className={styles.icon} width={25} height={25} alt='' />
-      </a>
-
-      <a href='' className={styles.card}>
-        <h2>Java Native Android Application &rarr;</h2>
-        <p>Android moblice application created with Java Spring. dfasdf dsaf df df ds fdfsdf sf ds.ef easf eaf efasfeasfaef gagergfsd</p>
-            <Image src={'/googlecloud.svg'} className={styles.icon} width={25} height={25} alt='' />
-            <Image src={'/ubuntu.svg'} className={styles.icon} width={25} height={25} alt='' />
-            <Image src={'/nginx.svg'} className={styles.icon} width={25} height={25} alt='' />
-            <Image src={'/intellijidea.svg'} className={styles.icon} width={25} height={25} alt='' />
-            <Image src={'/android.svg'} className={styles.icon} width={25} height={25} alt='' />
-            <Image src={'/postgres.svg'} className={styles.icon} width={25} height={25} alt='' />
-      </a>
-
-      <a href='' className={styles.card}>
-        <h2>Javascript Login and Authorization &rarr;</h2>
-        <p>Bug tracker project featuring login and autho, realtime information, ablity to crud message comment post. </p>
-            <Image src={'/googlecloud.svg'} className={styles.icon} width={25} height={25} alt='' />
-            <Image src={'/ubuntu.svg'} className={styles.icon} width={25} height={25} alt='' />
-            <Image src={'/nginx.svg'} className={styles.icon} width={25} height={25} alt='' />
+            <Image src={'/react.svg'} className={styles.icon} width={25} height={25} alt='' />
+            <Image src={'/nextdotjs.svg'} className={styles.icon} width={25} height={25} alt='' />
+            <Image src={'/typescript.svg'} className={styles.icon} width={25} height={25} alt='' />
             <Image src={'/html5.svg'} className={styles.icon} width={25} height={25} alt='' />
             <Image src={'/css3.svg'} className={styles.icon} width={25} height={25} alt='' />
-            <Image src={'/javascript.svg'} className={styles.icon} width={25} height={25} alt='' />
-            <Image src={'/nodedotjs.svg'} className={styles.icon} width={25} height={25} alt='' />
-            <Image src={'/npm.svg'} className={styles.icon} width={25} height={25} alt='' />
-            <Image src={'/postgres.svg'} className={styles.icon} width={25} height={25} alt='' />
       </a>
 
       <a href='' className={styles.card}>
-        <h2>Python Data Cleaning &rarr;</h2>
-        <p>Clean and organize Postgres data with SQL. data sourced from fdsaf dsaf dsaf dsaf sdf dsafag asdgawegfagsdg adsgfesagfsdgf eatgds  </p>
+        <h2>NASA Picture of the Day - Terminal Application &rarr;</h2>
+        <p>agsdag sdag sag dsgdsagasd gasdgsdagsdagdsag. dfasdf dsaf df df ds fdfsdf sf ds.ef easf eaf efasfeasfaef gagergfsd</p>
+          <Image src={'/windows.svg'} className={styles.icon} width={25} height={25} alt='' />
+          <Image src={'/docker.svg'} className={styles.icon} width={25} height={25} alt='' />
+          <Image src={'/git.svg'} className={styles.icon} width={25} height={25} alt='' />
+          <Image src={'/python.svg'} className={styles.icon} width={25} height={25} alt='' />
+      </a>
+
+      <a href='' className={styles.card}>
+        <h2>NASA Picture of the Day - Desktop Application &rarr;</h2>
+        <p>agsdag sdag sag dsgdsagasd gasdgsdagsdagdsag. dfasdf dsaf df df ds fdfsdf sf ds.ef easf eaf efasfeasfaef gagergfsd</p>
+          <Image src={'/windows.svg'} className={styles.icon} width={25} height={25} alt='' />
+          <Image src={'/docker.svg'} className={styles.icon} width={25} height={25} alt='' />
+          <Image src={'/git.svg'} className={styles.icon} width={25} height={25} alt='' />
+          <Image src={'/python.svg'} className={styles.icon} width={25} height={25} alt='' />
+      </a>
+
+      <a href='' className={styles.card}>
+        <h2>Text Editor &rarr;</h2>
+        <p>agsdag sdag sag dsgdsagasd gasdgsdagsdagdsag. dfasdf dsaf df df ds fdfsdf sf ds.ef easf eaf efasfeasfaef gagergfsd</p>
+            <Image src={'/windows.svg'} className={styles.icon} width={25} height={25} alt='' />
+            <Image src={'/vscode.svg'} className={styles.icon} width={25} height={25} alt='' />
+            <Image src={'/pandas.svg'} className={styles.icon} width={25} height={25} alt='' />
+      </a>
+
+      <a href='' className={styles.card}>
+        <h2>Hyperskill Project Repository &rarr;</h2>
+        <p>agsdag sdag sag dsgdsagasd gasdgsdagsdagdsag. dfasdf dsaf df df ds fdfsdf sf ds.ef easf eaf efasfeasfaef gagergfsd</p>
+            <Image src={'/intellijidea.svg'} className={styles.icon} width={25} height={25} alt='' />
+            <Image src={'/spring.svg'} className={styles.icon} width={25} height={25} alt='' />
+            <Image src={'/intellijidea.svg'} className={styles.icon} width={25} height={25} alt='' />
             <Image src={'/python.svg'} className={styles.icon} width={25} height={25} alt='' />
-            <Image src={'/postgres.svg'} className={styles.icon} width={25} height={25} alt='' />
+            <Image src={'/intellijidea.svg'} className={styles.icon} width={25} height={25} alt='' /> 
+            <Image src={'/javascript.svg'} className={styles.icon} width={25} height={25} alt='' />
+            <Image src={'/intellijidea.svg'} className={styles.icon} width={25} height={25} alt='' /> 
+            <Image src={'/mysql.svg'} className={styles.icon} width={25} height={25} alt='' />
+      </a>
+
+      <a href='' className={styles.card}>
+        <h2>Project Heading &rarr;</h2>
+        <p>agsdag sdag sag dsgdsagasd gasdgsdagsdagdsag. dfasdf dsaf df df ds fdfsdf sf ds.ef easf eaf efasfeasfaef gagergfsd</p>
+        <Image src={'/favicon-32x32.png'} className={styles.icon} width={25} height={25} alt='' />
+            <Image src={'/favicon-32x32.png'} className={styles.icon} width={25} height={25} alt='' />
+            <Image src={'/favicon-32x32.png'} className={styles.icon} width={25} height={25} alt='' />
+            <Image src={'/favicon-32x32.png'} className={styles.icon} width={25} height={25} alt='' />
+            <Image src={'/favicon-32x32.png'} className={styles.icon} width={25} height={25} alt='' />
+            <Image src={'/favicon-32x32.png'} className={styles.icon} width={25} height={25} alt='' />
+            <Image src={'/favicon-32x32.png'} className={styles.icon} width={25} height={25} alt='' />
+            <Image src={'/favicon-32x32.png'} className={styles.icon} width={25} height={25} alt='' />
+            <Image src={'/favicon-32x32.png'} className={styles.icon} width={25} height={25} alt='' />
+            <Image src={'/favicon-32x32.png'} className={styles.icon} width={25} height={25} alt='' />
+            <Image src={'/favicon-32x32.png'} className={styles.icon} width={25} height={25} alt='' />
+            <Image src={'/favicon-32x32.png'} className={styles.icon} width={25} height={25} alt='' />
+            <Image src={'/favicon-32x32.png'} className={styles.icon} width={25} height={25} alt='' />
+      </a>
+
+      <a href='' className={styles.card}>
+        <h2>Project Heading &rarr;</h2>
+        <p>agsdag sdag sag dsgdsagasd gasdgsdagsdagdsag. dfasdf dsaf df df ds fdfsdf sf ds.ef easf eaf efasfeasfaef gagergfsd</p>
+            <Image src={'/favicon-32x32.png'} className={styles.icon} width={25} height={25} alt='' />
+            <Image src={'/favicon-32x32.png'} className={styles.icon} width={25} height={25} alt='' />
+            <Image src={'/favicon-32x32.png'} className={styles.icon} width={25} height={25} alt='' />
+            <Image src={'/favicon-32x32.png'} className={styles.icon} width={25} height={25} alt='' />
+            <Image src={'/favicon-32x32.png'} className={styles.icon} width={25} height={25} alt='' />
+            <Image src={'/favicon-32x32.png'} className={styles.icon} width={25} height={25} alt='' />
+            <Image src={'/favicon-32x32.png'} className={styles.icon} width={25} height={25} alt='' />
+            <Image src={'/favicon-32x32.png'} className={styles.icon} width={25} height={25} alt='' />
+            <Image src={'/favicon-32x32.png'} className={styles.icon} width={25} height={25} alt='' />
+            <Image src={'/favicon-32x32.png'} className={styles.icon} width={25} height={25} alt='' />
+            <Image src={'/favicon-32x32.png'} className={styles.icon} width={25} height={25} alt='' />
+            <Image src={'/favicon-32x32.png'} className={styles.icon} width={25} height={25} alt='' />
+            <Image src={'/favicon-32x32.png'} className={styles.icon} width={25} height={25} alt='' />
       </a>
 
     </div>
